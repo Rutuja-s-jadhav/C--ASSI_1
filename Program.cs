e98ffb4f84da5b574de30753adad7bd0d13dd3c3
Rutuja-s-jadhav <115146395+Rutuja-s-jadhav@users.noreply.github.com>
2022-10-28 08:17:43 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,49 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace calculator
+namespace assi_1._2
 {
     class Program
     {
-        static void Main(string[] args)
+        public class program
         {
-            char op;
-            double num1, num2;
-
-            Console.WriteLine("Enter the operator(+,-,*,/)");
-           op = Console.ReadLine()[0];
-
-            Console.WriteLine("Enter the two numbers one by one");
-           num1 = Convert.ToDouble(Console.ReadLine());
-            num2 = Convert.ToDouble(Console.ReadLine());
-
-            switch( op )
+            static void main(string[] args)
             {
-                case '+':
-                    Console.WriteLine("{0}+{1}={2}", num1, num2, (num1 + num2));
-                    break;
-
-                case '-':
-                    Console.WriteLine("{0}-{1}={2}", num1, num2, (num1 - num2));
-                    break;
-
-                case '*':
-                    Console.WriteLine("{0}*{1}={2}", num1, num2, (num1 * num2));
-                    break;
-
-                case '/':
-                    if (num2 == 0.0)
-                        Console.WriteLine("divide by zero situation");
-                    else
-                        Console.WriteLine("{0}/{1}={2}", num1, num2, (num1 / num2));
-                    break;
-
-                default:
-                    Console.WriteLine("{0} is an invalid operator", op);
-                    break;
+                Console.WriteLine("Average marks of 5 students: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("please enter marks for 2nd student: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("please enter marks for 3rd student: ");
+                int c = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("please enter marks for 4th student: ");
+                int d = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("please enter marks for 5th student: ");
+                int e = Convert.ToInt32(Console.ReadLine());
+                int[] Average = { a, b, c, d, e };
+                Console.WriteLine("maximum marks obtained are: " + Average.Max());
+                Console.ReadKey();
             }
-
-            Console.WriteLine("press any key to exit the program");
-            Console.ReadKey();
         }
     }
 }
